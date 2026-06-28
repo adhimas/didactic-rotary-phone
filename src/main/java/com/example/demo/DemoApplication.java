e4442d9e448881255e5d1341ae0f7817f7701b84
adhimas <adhimas@users.noreply.github.com>
2026-06-28 12:27:24 +0200

    Rename

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.doodle;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoodleApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoodleApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
