3531ea52fb34066638998dde03c80d108bf6ae60
adhimas <adhimas@users.noreply.github.com>
2026-06-28 13:23:10 +0200

    Remove unused imports

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,7 +1,6 @@
 package com.example.demo.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.random.RandomGenerator;
 
 @Service
 @RequiredArgsConstructor
